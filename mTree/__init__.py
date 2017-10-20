6bc41a9cfc4619ec938eeaa7fbf193a29b14722c
S. Kunath <skunath@someplace.com>
2017-10-20 15:55:52 -0400

    Fixes for pypi distribution

--- mTree/__init__.py
+++ mTree/__init__.py
@@ -3,3 +3,5 @@ from .base import *
 from .base.agents import *
 from .experiment import *
 from .new_objects import *
+from .microeconomic_system import *
+from .templates import *
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="mTree",
-    version="0.2-BETA",
+    version="0.2-BETA-1",
     packages=find_packages(),
     include_package_data=True,
     # Project uses reStructuredText, so ensure that the docutils get
